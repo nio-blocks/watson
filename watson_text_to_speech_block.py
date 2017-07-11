1fb62a0bdbd419110aef81e942c49624f29a33dd
Dylan Orzel <dylan.orzel@gmail.com>
2017-07-11 15:46:13 -0600

    add watson text to speech block

---
+++ watson_text_to_speech_block.py
@@ -0,0 +1,46 @@
+import uuid
+from enum import Enum
+from watson_developer_cloud import TextToSpeechV1
+
+from nio.block.base import Block
+from nio.properties import (VersionProperty, Property, FileProperty,
+                            SelectProperty)
+
+
+class Voices(Enum):
+    enUSLisa = "en-US_LisaVoice"
+
+
+class WatsonTextToSpeech(Block):
+
+    version = VersionProperty('1.0.0')
+    username = Property(title='Username', default='')
+    password = Property(title='Password', default='')
+    data_attr = Property(title='Text to Convert',
+                         default='{{ $text }}')
+    speech_file_location = FileProperty(title='Directory to save audio files',
+                                        default='etc/')
+    voice = SelectProperty(Voices, default=Voices.enUSLisa, title="Voice")
+
+    def __init__(self):
+        self.tts_engine = None
+        super().__init__()
+
+    def start(self):
+        self.tts_engine = TextToSpeechV1(username=self.username(),
+                                         password=self.password())
+        super().start()
+
+    def process_signals(self, signals):
+        for signal in signals:
+            try:
+                filename = str(uuid.uuid4()) + ".wav"
+                with open(self.speech_file_location() + filename, 'wb') as audio_file:
+                    audio_file.write(
+                        self.tts_engine.synthesize(text=self.data_attr(signal),
+                                                   voice=self.voice().value,
+                                                   accept="audio/wav"))
+            except Exception:
+                self.logger.exception("Failed to write speech file: ")
+            else:
+                self.logger.info("Wrote speech file to {}")
